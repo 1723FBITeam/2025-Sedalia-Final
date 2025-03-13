86c4e8e95ed31aa7038f713be2a3c94f2f35ed19
1723FBI <1723fbirobotics@gmail.com>
2025-03-13 17:38:18 -0500

    update elevator

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -30,7 +30,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     private Map<Integer, Double> targetPositions = Map.of(
             0, 2.2,
             1, 23.5,
-            2, 31.0,
+            2, 32.0,
             3, 39.0);
     private int currentPositionKey = 0;
 
